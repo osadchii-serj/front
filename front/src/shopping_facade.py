4517783e8982f18cf946ce90c4e7340e8f7d7a3e
Сергей <osedchii.serega48@gmail.com>
2024-10-02 21:50:57 +0300

    ShoppingFacade add warehouse_service, users_base_service, payment_service, product_service, delivery_service, order_service, user_service

--- front/src/shopping_facade.py
+++ front/src/shopping_facade.py
@@ -1,5 +1,21 @@
 from dataclasses import dataclass
 
+from warehouse import Warehouse
+from users_base import UsersBase
+from payment import Payment
+from product import Product
+from delivery import Delivery
+from order import Order
+from user import User
 
+
+@dataclass
 class ShoppingFacade:
-    pass
+
+    warehouse_service = Warehouse
+    users_base_service = UsersBase
+    payment_service = Payment
+    product_service = Product
+    delivery_service = Delivery
+    order_service = Order
+    user_service = User
