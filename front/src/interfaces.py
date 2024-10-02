1096b8455eecb168598a4ae00560103fa43cafd4
Сергей <osedchii.serega48@gmail.com>
2024-10-02 16:05:00 +0300

    add IUser

--- front/src/interfaces.py
+++ front/src/interfaces.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+
+from dataclasses import dataclass
+
+from typing import Dict, List
 
 
 class IUsersBase(ABC):
 
-    users_base = {}
+    users_base: Dict[str] = {}
 
     @abstractmethod
     def add_user(self, user: Dict[str]):
@@ -20,3 +23,24 @@ class IUsersBase(ABC):
 
 
 # =======================================================================================================================
+
+
+@dataclass
+class IUser(ABC):
+
+    user_name: str = None
+    user_email: str = None
+    user_password: str = None
+    user_id = None
+    user_cart = []
+
+    @abstractmethod
+    def create_user_id(self):
+        pass
+
+    @abstractmethod
+    def add_product_cart(self, product):
+        pass
+
+
+# ========================================================================================================================
